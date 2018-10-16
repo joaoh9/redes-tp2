c18be01861cce05f43899630349656771995fecb
Joao Henrique <joaoh9.costa@gmail.com>
2018-10-16 17:19:19 -0300

    thread para listen + connect no comando add

--- router.py
+++ router.py
@@ -5,9 +5,10 @@ import re
 import socket
 
 routes = {}
+PORT = 55151
+addr = None
 
-
-def add_link(receiver, distance):
+def add_link(receiver, distance, sock):
     if receiver not in routes:
         routes[receiver] = [[receiver, distance]]
     else:
@@ -16,6 +17,7 @@ def add_link(receiver, distance):
                 print("Route " + receiver + " already added.")
                 return
         routes[receiver].append[[receiver, distance]]
+        sock.connect((receiver, PORT))
     print("Added route to " + receiver + " with distance " + str(distance))
 
 
@@ -25,14 +27,14 @@ def del_enlace(enlace):
     print("Del enlace")
 
 
-def prompt():
+def prompt(sock, addr):
     while 1:
         s = input("> ")
         cmd = s.split()
         if len(cmd) is 0:
             continue
         elif cmd[0] == "add":
-            add_link(cmd[1], int(cmd[2]))
+            add_link(cmd[1], int(cmd[2]), sock)
         elif cmd[0] == "del":
             print("Run del")
         elif cmd[0] == "trace":
@@ -44,11 +46,25 @@ def prompt():
             output3 = re.sub(r'",\s+', '", ', output2)
             output4 = re.sub(r'"\s+\]', '"]', output3)
             print(output4)
+        elif cmd[0] == "data":
+            data = cmd[1]
+            dest = cmd[2]
+            send_data(data, sock, addr)
         elif cmd[0] == "quit":
             break
         else:
             print("Command not found")
 
+#def send_data(data, sock, dest):
+#    json.dumps({"type": "data", "source": addr, "destination": dest , "payload": data})
+
+
+def listen(sock):
+    sock.listen()
+    udpsock, addr = sock.accept()
+    print ('Connected to router ' + str(addr))
+    #while 1:
+    data = udpsock.recv(1024)
 
 def main():
     addr = sys.argv[1]
@@ -62,12 +78,16 @@ def main():
 
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((addr, port))
-
-    t_prompt = threading.Thread(target=prompt, args=())
+    sock.bind((addr, PORT))
+    
+    t_prompt = threading.Thread(target=prompt, kwargs={'sock': sock, 'addr': addr})
     t_prompt.start()
-    t_prompt.join()
 
+    t_listen = threading.Thread(target=listen, kwargs={'sock': sock})
+    t_listen.start()
+
+    t_prompt.join()
+    
 
 if __name__ == "__main__":
     main()
