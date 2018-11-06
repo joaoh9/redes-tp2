ca2b08452b22cc08f3a8cfe70529a1a744e89629
José Oliveira <josecarlosdeoliveirajr@gmail.com>
2018-11-06 15:23:40 -0200

    Do trace.

--- router.py
+++ router.py
@@ -9,6 +9,112 @@ import time
 PORT = 55151
 
 
+def read_file(startup_file):
+    """
+    Read startup file and add links on table.
+    :param startup_file:
+    :type startup_file: TextIO
+    """
+    while True:
+        line = startup_file.readline().replace("\n", "")
+        if len(line) == 0:
+            break
+        cmd = line.split()
+        if cmd[0] == "add":
+            table.add_link(cmd[1], int(cmd[2]))
+
+
+def data_handler(packet):
+    """
+    Handler packets for data received.
+    :param packet: Packet received.
+    :type packet: dict
+    """
+    if packet["destination"] == ADDR:
+        print(packet["payload"])
+    else:
+        send_packet(packet)
+
+
+def update_handler(packet):
+    """
+    Handler packets for periodic update.
+    :param packet: Packet received.
+    :type packet: dict
+    """
+    payload = packet['payload']
+    source = packet['source']
+    if source not in table.routes:
+        print("There is not " + source + " in table.")
+        return
+    distance_from_source = table.routes[source].min
+    for key in payload:
+        if key != ADDR:
+            table.add_learned_router(key, distance_from_source + int(payload[key]), source)
+
+
+def send_packet(packet):
+    """
+    Send a dict packet to a router.
+    :param packet: Dict packet with info.
+    :type packet: dict
+    """
+    destination = table.get_destination_by_routes(packet['destination'])
+    json_packet = json.dumps(packet)
+    message = bytes(json_packet, 'utf-8')
+    SOCK.sendto(message, (destination, PORT))
+
+
+def trace_handler(packet):
+    """
+    Handler packets for trace command.
+    :param packet: Packet received.
+    :type packet: dict
+    """
+    packet["hops"].append(ADDR)
+    if packet["destination"] != ADDR:
+        send_packet(packet)
+    else:
+        payload = json.dumps(packet)
+        data_packet = create_data_packet(packet["source"], payload)
+        send_packet(data_packet)
+
+
+def recv_packet():
+    """
+    Thread for receive packets and handle.
+    """
+    while True:
+        msg, ip = SOCK.recvfrom(1024)
+        if len(msg) > 0:
+            msg = bytes.decode(msg)
+            packet = json.loads(msg)
+            if packet["type"] == "update":
+                update_handler(packet)
+            elif packet["type"] == "data":
+                data_handler(packet)
+            elif packet["type"] == "trace":
+                trace_handler(packet)
+            # treat_function = get_treater(packet["type"])
+            # treat_function(packet)
+
+
+def create_trace_packet(destination):
+    """
+    Create a dictionary for trace to be send.
+    :param destination: Router destination IP address.
+    :type destination: str
+    :return: Dictionary.
+    :rtype: dict
+    """
+    return {
+        "type": "trace",
+        "source": ADDR,
+        "destination": destination,
+        "hops": [ADDR]
+    }
+
+
 def create_data_packet(destination, message):
     """
     Create a dictionary with data in a packet to be send.
@@ -26,72 +132,12 @@ def create_data_packet(destination, message):
         "payload": message
     }
 
-def treat_data(packet):
-    print(packet['payload'])
-
-
-def create_update_packet(destination, payload):
-    return {
-        "type": "update",
-        "source": ADDR,
-        "destination": destination,
-        "payload": payload
-    }
-
-
-def treat_update(packet):
-    payload = packet['payload']
-    source = packet['source']
-    #print('update coming fron: ', source)
-    #print('update payload: ', payload)
-    if source not in table.routes:
-        print("There is not " + source + " in table.")
-        return
-
-    distance_from_source = table.routes[source].min
-    
-    # print('got this payload: ' + str(payload) + ' from :' + source)
-    for key in payload:
-        if key != ADDR:
-            table.add_learned_router(key, distance_from_source + int(payload[key]), source)
-
-
-def get_treater(type):
-    treat_function = {
-        "data": treat_data,
-        "update": treat_update
-        #Q"trace": treat_trace
-    }
-    return treat_function.get(type)
-
-
-def send_packet(packet):
-    """
-    Send a dict packet to a router.
-    :param packet: Dict packet with info.
-    :type packet: dict
-    """
-    destination = table.get_destination_by_routes(packet['destination'])
-    json_packet = json.dumps(packet)
-    message = bytes(json_packet, 'utf-8')
-    SOCK.sendto(message, (destination, PORT))
-
-
-def recv_packet():
-    while True:
-        msg, ip = SOCK.recvfrom(1024)
-        if len(msg) > 0:
-            msg = bytes.decode(msg)
-            packet = json.loads(msg)
-            treat_function = get_treater(packet["type"])
-            treat_function(packet)
-
 
 def prompt():
     """
     Read commands and select the right function.
     """
-    while 1:
+    while True:
         s = input()
         cmd = s.split()
         if len(cmd) is 0:
@@ -110,24 +156,34 @@ def prompt():
             if len(cmd) < 2:
                 print("Too few arguments")
                 continue
-            print("Run trace")
+            destination = cmd[1]
+            packet = create_trace_packet(destination)
+            send_packet(packet)
         elif cmd[0] == "status":
             print(table.to_string())
         elif cmd[0] == "msg":
             if len(cmd) < 3:
                 print("Too few arguments")
                 continue
-            receiver = cmd[1]
-            message = cmd[2]
-            packet = create_data_packet(receiver, message)
+            destination = cmd[1]
+            message = " ".join(cmd[2:])
+            packet = create_data_packet(destination, message)
             send_packet(packet)
-            print("send msg")
         elif cmd[0] == "help":
             print("add del trace data help quit")
         elif cmd[0] == "quit":
-            break
+            sys.exit(0)
         else:
             print("Command not found")
+
+
+def create_update_packet(destination, payload):
+    return {
+        "type": "update",
+        "source": ADDR,
+        "destination": destination,
+        "payload": payload
+    }
 
 
 def update(period):
@@ -152,19 +208,6 @@ def update(period):
                         
         # print(table.to_string())
         time.sleep(period)
-
-
-def read_file(startup_file):
-    while True:
-        line = startup_file.readline().replace("\n", "")
-        if len(line) == 0:
-            break
-        cmd = line.split()
-        
-        if cmd[0] == "add":
-            table.add_link(cmd[1], int(cmd[2]))
-    print('Finished reading file')
-    return
 
 
 def remove_outdated_routes(period):
@@ -204,8 +247,7 @@ def main():
 
     global table
     table = Table()
-    
-    # --startup-comands
+
     if len(sys.argv) > 3:
         startup = sys.argv[3]
         startup_file = open(startup, "r")
